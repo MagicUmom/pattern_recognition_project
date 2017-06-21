ee876704ffaa793261259117fa6ba67a0e400261
Unknown <ken51006b@hotmail.com>
2017-06-21 19:11:40 +0800

    #1 edit something

--- svm_line.py
+++ svm_line.py
@@ -5,20 +5,19 @@ print(__doc__)
 
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
+import Image
 
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 
 # The digits dataset
-digits = datasets.load_digits()
+# digits = datasets.load_digits()
 
-# The data that we are interested in is made of 8x8 images of
-# digits, let's # have a look at the first 3 images, stored in
-# the `images` attribute of the # dataset.  If we were working
-# from image files, we could load them using # pylab.imread.
-# Note that each image must have the same size. For these  images,
-#we know which digit they represent: it is given in the 'target' of
-# the dataset.
+dataset = 
+
+
+
+
 images_and_labels = list(zip(digits.images, digits.target))
 for index, (image, label) in enumerate(images_and_labels[:4]):
     plt.subplot(2, 4, index + 1)
